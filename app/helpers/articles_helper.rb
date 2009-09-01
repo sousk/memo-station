eddee0a4290347e25a747dcb56f01da73de0f0cc
sousk.net <sou.phphp@gmail.com>
2009-09-02 06:40:18 +0900

    update

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,5 +5,5 @@ module ArticlesHelper
   
   def latest_articles
     Article.find(:all, :order => "created_at DESC", :limit => 10)
-  end
+  end  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,25 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+  has_many :article_view_logs, :order => "created_at DESC"
+  has_many :users, :through => :article_view_logs
+
+  # acts_as_taggable :join_table => "tags_articles"
+
+  validates_presence_of :subject, :user_id, :my_tags
+  validates_uniqueness_of :subject
+
+  delegate :logger, :to => "self.class"
+  delegate :debug, :to => "self.class.logger"
+  
+  
+  class << self
+    def latest(limit=10)
+      Article.find(:all, :order => "created_at DESC", :limit => limit)
+    end
+    
+    def count_tag(limit=50)
+      # Article.tags_count(:count => ">= 2", :limit => limit)
+    end
+  end
 end
+
