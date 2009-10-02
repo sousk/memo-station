7af4e788aa345a7d157e00286166268b4fc56485
sousk <sou.phphp@gmail.com>
2009-10-02 19:07:00 +0900

    update rails ver

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mislav-will_paginate', 
     :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem 'rmagick'
   config.gem "locale"
   config.gem "locale_rails"
   config.gem "gettext"
